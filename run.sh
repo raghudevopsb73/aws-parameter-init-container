35e36376e2ea86496cf47af98e0849d4368c93fd
Raghu K <raghuk.vit@gmail.com>
2023-08-05 07:59:30 +0530

    Update aws-parameter-init-container/

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-aws ssm describe-parameters
+for param in ${PARAMETERS} ; do
+  PASS=$(aws ssm get-parameter --name $param --with-decryption --query 'Parameter.Value' --output text)
+  echo export $param=$PASS >>params
+done
+
+cat params
